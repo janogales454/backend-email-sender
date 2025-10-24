865b0ce197cbda35ad8d8fde5ee35ef2eb6722fa
janogales454 <jacobnogales454@gmail.com>
2025-10-24 02:41:20 -0400

    Uploaded initial project code

---
+++ src/main/java/com/janogales/BackendEmailSender/BackendEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.janogales.BackendEmailSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janogales/BackendEmailSender/BackendEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janogales.BackendEmailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
